3a2f73e1d602e82318e1cfeca4e8025c07f1fc3d
Ehayes072 <erichayes711@gmail.com>
2026-09-16 16:57:45 +0000

    chore: work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+	fn is_even(n: i32) -> bool {
+n % 2 == 0
+}
+
+let n = 4;
+
+if is_even(n) { % 2 == 0{
+	println("{n} is even");
+}else{
+	println!("{num} is odd");
+}
+
 }
