79655a0aea133e7ed0672ba07a92a0fbd9352345
patricksengaroun <patricksengaroun@gmail.com>
2026-09-21 16:59:12 +0000

    added: sum_digit

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,28 @@ fn main() {
 let n = 42;
 println!("Is {} even? {}", n, is_even(n));
 
+	let num = 12345;
+	println!("Sum of digits: {}", sum_digits(num));
 }
  
 	fn is_even(n: i32) -> bool { 
 n % 2 == 0
 }
+
+fn next() {
+}
+
+fn sum_digits(num: i32) -> u32 {
+	let mut current = num.unsigned_abs();
+	let mut sum: u32 = 0;
+
+	while current > 0 {
+		sum += current % 10;
+		current /= 10;
+	}
+
+	sum
+}
+
+
+
