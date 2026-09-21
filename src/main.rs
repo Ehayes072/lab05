f80160eca4bdb6c1bd5dc693c64f1b73ff9b7714
Ehayes072 <erichayes711@gmail.com>
2026-09-21 16:33:54 +0000

    feat: added is_number even

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 fn main() {
-	fn is_even(n: i32) -> bool {
+let n = 42;
+println!("Is {} even? {}", n, is_even(n));
+
+}
+ 
+	fn is_even(n: i32) -> bool { 
 n % 2 == 0
-}
-
-let n = 4;
-
-if is_even(n) { % 2 == 0{
-	println("{n} is even");
-}else{
-	println!("{num} is odd");
-}
-
 }
